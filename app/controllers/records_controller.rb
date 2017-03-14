95b4576d9e1912132365ff1bde0c689e37ea388e
darkness <gavrileypetro@gmail.com>
2017-03-14 15:47:17 +0200

    corrects records controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,28 +4,22 @@ class RecordsController < ApplicationController
   def index
     @record = current_user.last_open_record
     @record = Record.new if @record.nil?
-    set_record_list
+
+    @records = current_user.records.closed
   end
 
   def create
     @record = current_user.records.build(started_at: Time.now)
     current_user.records << @record if @record.save
 
-    set_record_list
     render :record
   end
 
   def update
-    @record = current_user.records.find(params[:id])
+    @last_record = @record = current_user.records.find(params[:id])
     @record = Record.new if @record.update(finished_at: Time.now)
 
-    set_record_list
     render :record
   end
 
-  private
-
-  def set_record_list
-    @records = current_user.records.closed
-  end
 end
