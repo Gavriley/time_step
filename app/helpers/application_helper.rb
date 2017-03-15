696e1529e50a74d67efb3ab26e60624fa032ea4e
darkness <gavrileypetro@gmail.com>
2017-03-15 14:24:34 +0200

    adds universal helper for flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def flash_message
+    if flash[:notice]
+      content_tag(:div, flash[:notice], class: 'alert alert-success')
+    elsif flash[:error]
+      content_tag(:div, flash[:error], class: 'alert alert-danger')
+    elsif flash[:alert]
+      content_tag(:div, flash[:alert], class: 'alert alert-warning')
+    end
+  end
+
 end
