341e96a6d9606b2f8e0fef5da34481148b75df41
darkness <gavrileypetro@gmail.com>
2017-03-15 17:04:00 +0200

    adds rescue from access denied and flash info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from CanCan::AccessDenied, with: :access_denied
+
+  private
+
+  def access_denied
+    flash[:info] = 'Access denied'
+    redirect_to root_url
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,12 @@ module ApplicationHelper
   def flash_message
     if flash[:notice]
       content_tag(:div, flash[:notice], class: 'alert alert-success')
-    elsif flash[:error]
-      content_tag(:div, flash[:error], class: 'alert alert-danger')
     elsif flash[:alert]
       content_tag(:div, flash[:alert], class: 'alert alert-warning')
+    elsif flash[:error]
+      content_tag(:div, flash[:error], class: 'alert alert-danger')
+    elsif flash[:info]
+      content_tag(:div, flash[:info], class: 'alert alert-info')
     end
   end
 
