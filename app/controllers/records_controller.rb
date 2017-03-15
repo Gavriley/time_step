6372930b05dcef55b6016d840a487bd6d5878d9b
darkness <gavrileypetro@gmail.com>
2017-03-15 13:48:01 +0200

    corrects records_controller test to slit it into many tests

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,27 +4,18 @@ class RecordsController < ApplicationController
   def index
     @record = current_user.last_open_record
     @record = Record.new if @record.nil?
-
     @records = current_user.records.closed
   end
 
   def create
     @record = current_user.records.build(started_at: Time.now)
-
-    if @record.save
-      current_user.records << @record
-    else
-      error_message
-    end
-
+    error_message unless @record.save
     redirect_to records_url
   end
 
   def update
     @record = current_user.records.find(params[:id])
-
     error_message unless @record.update(finished_at: Time.now)
-
     redirect_to records_url
   end
 
--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -1,20 +1,34 @@
 require 'rails_helper'
 
 describe RecordsController do
-  render_views
-
   let(:user) { FactoryGirl.create(:user) }
 
   before(:each) { sign_in user }
 
-  it 'starting and finishing work' do
-    post :create, params: { record: {}, format: :js }
-    puts user.records.last.finished_at
-    expect(response).to redirect_to records_url
-    put :update, params: { id: user.records.last.id, record: {}, format: :js }
-    expect(response).to redirect_to records_url
+  it 'GET index' do
     get :index
-    expect(response.body).to match user.records.last.started_at.strftime('%d %B %Y %H:%M:%S')
-    expect(response.body).to match user.records.last.finished_at.strftime('%d %B %Y %H:%M:%S')
+    expect(response).to have_http_status 200
+  end
+
+  it 'POST create' do
+    post :create, params: { record: {}, format: :js }
+    expect(response).to redirect_to records_url
+  end
+
+  it 'PUT update' do
+    started_record = user.records.create(started_at: Time.now)
+    put :update, params: { id: started_record.id, record: {}, format: :js }
+    expect(response).to redirect_to records_url
+  end
+
+  context 'view response' do
+    render_views
+
+    it 'GET index' do
+      finished_record = user.records.create(started_at: Time.now, finished_at: Time.now + 8.hours)
+      get :index
+      expect(response.body).to match finished_record.started_at.strftime('%d %B %Y %H:%M:%S')
+      expect(response.body).to match finished_record.finished_at.strftime('%d %B %Y %H:%M:%S')
+    end
   end
 end
